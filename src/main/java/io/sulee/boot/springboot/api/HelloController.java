eacfa945b8e190b1fbabe7ea75dc920063613321
sulee <tnwk09@gmail.com>
2018-09-19 19:25:35 +0900

    0919

---
+++ src/main/java/io/sulee/boot/springboot/api/HelloController.java
@@ -0,0 +1,13 @@
+package io.sulee.boot.springboot.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return "Hello, Boot Spring Boot!!";
+    }
+}
