d2259d074c20acc588a0b1ebc93056524d717dd1
suyoun <tnwk09@gmail.com>
2018-09-28 03:51:17 +0900

    0928(2)

--- src/main/java/io/sulee/boot/springboot/support/AppArgsAccessor.java
+++ src/main/java/io/sulee/boot/springboot/support/AppArgsAccessor.java
@@ -1,24 +1,24 @@
-package io.sulee.boot.springboot.support;
-
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-@Slf4j
-@Component
-public class AppArgsAccessor  {
-
-    @Autowired
-    public AppArgsAccessor(ApplicationArguments args){
-        boolean debug = args.containsOption("debug");
-        log.debug("debug={}", debug);
-        List<String> files = args.getNonOptionArgs();
-        log.debug("files={}", files);
-        // "--debug logfile.txt" 라는 실행인자를 주어 애플리케이션을 실행하면 debug=true, files=["logfile.txt"]를 확인할 수 있다.
-
-    }
-}
+//package io.sulee.boot.springboot.support;
+//
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+//@Slf4j
+//@Component
+//public class AppArgsAccessor  {
+//
+//    @Autowired
+//    public AppArgsAccessor(ApplicationArguments args){
+//        boolean debug = args.containsOption("debug");
+//        log.debug("debug={}", debug);
+//        List<String> files = args.getNonOptionArgs();
+//        log.debug("files={}", files);
+//        // "--debug logfile.txt" 라는 실행인자를 주어 애플리케이션을 실행하면 debug=true, files=["logfile.txt"]를 확인할 수 있다.
+//
+//    }
+//}
